43ab9cdd4b3ce077b97e3c8b86e3c3df4f639f7a
VtheWizard <villepri@hotmail.com>
2025-01-20 14:15:21 +0200

    a moving rectangle has appeared
    a moving rectgangle. slapped a picture on a sprite and there is some text that i managed to get to display the dimensions of the window.

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,12 +1,93 @@
-import { Application } from "pixi.js";
+import { Application, Graphics, Text, TextStyle, Sprite, Assets } from "pixi.js";
 
 (async() => {
 
 const app = new Application();
 
-await app.init();
+await app.init({
+    //width: 800,
+    //height: 600,
+    resizeTo: window,
+    backgroundColor: 0x1099bb,
+    backgroundAlpha: 0.9
+});
+app.stage.sortableChildren = true;
+app.canvas.style.position = "absolute";
+
+const maxHeight = window.innerHeight - 5;
+const maxWidht = window.innerWidth - 5;
+const speed = 10;
+const faster = 5;
+
+app.ticker.add(() => {
+    if (rectangle.x < 0){
+        rectangle.x = maxWidht / 2 - rectangle.width / 2;
+    }
+    if (rectangle.x > maxWidht - rectangle.width){
+        rectangle.x = maxWidht / 2 - rectangle.width / 2;
+    }
+    if (rectangle.y < 0){
+        rectangle.y = maxHeight/2 - rectangle.height/2;
+    }
+    if (rectangle.y > maxHeight- rectangle.height){
+        rectangle.y = maxHeight/2 - rectangle.height/2;
+    }
+    
+});
+
+window.addEventListener("keydown", (event)=>{
+    if(event.key === "ArrowUp"){
+        rectangle.y -= speed;
+    }
+    if(event.key === "ArrowDown"){
+        rectangle.y += speed;
+    }
+    if(event.key === "ArrowLeft"){
+        rectangle.x -= speed;
+    }
+    if(event.key === "ArrowRight"){
+        rectangle.x += speed;
+    }
+});
+
+const style = new TextStyle({
+    fontFamily: "Comic Sans MS",
+    fontSize: 34,
+    fill: 0xffffff,
+    stroke: 0x000000,
+    strokeThickness: 3
+});
+
+const text = new Text({
+    text: "window.width = " + maxWidht + " / window.height = " + maxHeight,
+    style: style,
+    position: {
+        x: 50,
+        y: 0
+    }
+});
+app.stage.addChild(text);
+
+const rectangle = new Graphics()
+    .rect(0, 0, 100, 125) //x,y,width,height
+    .fill({
+    color: 0xff0000,
+    alpha: 0.9
+    })
+    .stroke({
+    color: 0x000000,
+    width: 6
+    });
+app.stage.addChild(rectangle);
+
+const texture = await Assets.load('/images/sesu.png');
+const sprite = Sprite.from(texture);
+//sprite.width = 200;
+//sprite.height = 200;
+sprite.scale = 0.25
+sprite.position._x = 50;
+sprite.position._y = 50;
+app.stage.addChild(sprite);
 
 document.body.appendChild(app.canvas);
-
-
 })();
