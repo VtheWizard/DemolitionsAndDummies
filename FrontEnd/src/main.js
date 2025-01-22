70b56fc2267fa20deab9ddb63aa9eb6c4ece0018
VtheWizard <villepri@hotmail.com>
2025-01-22 21:03:07 +0200

    movement with switch/case and some cleanup
    swapped movements to switch/case and cleaned up some of the stuff that were there only for testing such as the horribly made collision. started on making a grid

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -13,87 +13,47 @@ await app.init({
 });
 app.stage.sortableChildren = true;
 app.canvas.style.position = "absolute";
+document.body.appendChild(app.canvas);
 
 const maxHeight = window.innerHeight - 5;
 const maxWidht = window.innerWidth - 5;
 const speed = 3;
+const gridSize = 50;
+const gridRows = 10;
+const gridCols = 10;
 let p1velocityX = 0;
 let p1velocityY = 0;
 let p2velocityX = 0;
 let p2velocityY = 0;
 let speedmodifier = 1;
-let playercount = 1;
-
+let localPlayerCount = 1;
 
 window.addEventListener("keydown", (event)=>{
     
-    //player 1 is on arrow keys
-    if(event.key === "ArrowUp"){
-        p1velocityY = -speed;
-    }
-    if(event.key === "ArrowDown"){        
-        p1velocityY = speed;
-    }
-    if(event.key === "ArrowLeft"){        
-        p1velocityX = -speed;
-    }
-    if(event.key === "ArrowRight"){        
-        p1velocityX = speed;
-    }
-
-    //player 2 is on "wasd"
-    if(event.key === "w"){
-        p2velocityY = -speed;
-    }
-    if(event.key === "s"){        
-        p2velocityY = speed;
-    }
-    if(event.key === "a"){        
-        p2velocityX = -speed;
-    }
-    if(event.key === "d"){        
-        p2velocityX = speed;
+    //player 1 is on arrow keys and player 2 on wasd
+    switch (event.key) {
+        case "ArrowUp": p1velocityY = -speed; break;
+        case "ArrowDown": p1velocityY = speed; break;
+        case "ArrowLeft": p1velocityX = -speed; break;
+        case "ArrowRight": p1velocityX = speed; break;
+        case "w": p2velocityY = -speed; break;
+        case "s": p2velocityY = speed; break;
+        case "a": p2velocityX = -speed; break;
+        case "d": p2velocityX = speed; break;
     }
 });
 
-function isColliding(rect, spr){
-    const intersects = rect.x < spr.x + spr.width &&
-        rect.x + rect.width > spr.x &&
-        rect.y < spr.y + spr.height &&
-        rect.y + rect.height > spr.y;
-    return intersects;
-}
-
-function newPlayer(){
-    app.stage.addChild(Player2);
-}
-
 window.addEventListener("keyup", (event)=>{
-    //player 1 on arrows
-    if(event.key === "ArrowUp"){        
-        p1velocityY = 0;
-    }
-    if(event.key === "ArrowDown"){        
-        p1velocityY = 0;
-    }
-    if(event.key === "ArrowLeft"){        
-        p1velocityX = 0;
-    }
-    if(event.key === "ArrowRight"){        
-        p1velocityX = 0;
-    }
-    //player 2 on wasd
-    if(event.key === "w"){        
-        p2velocityY = 0;
-    }
-    if(event.key === "s"){        
-        p2velocityY = 0;
-    }
-    if(event.key === "a"){        
-        p2velocityX = 0;
-    }
-    if(event.key === "d"){        
-        p2velocityX = 0;
+    //player 1 on arrows and player 2 on wasd
+    switch (event.key) {
+        case "ArrowUp": p1velocityY = 0; break;
+        case "ArrowDown": p1velocityY = 0; break;
+        case "ArrowLeft": p1velocityX = 0; break;
+        case "ArrowRight": p1velocityX = 0; break;
+        case "w": p2velocityY = 0; break;
+        case "s": p2velocityY = 0; break;
+        case "a": p2velocityX = 0; break;
+        case "d": p2velocityX = 0; break;
     }
 });
 
@@ -115,7 +75,7 @@ const text = new Text({
 });
 app.stage.addChild(text);
 
-const rectangle = new Graphics()
+const Player1 = new Graphics()
     .rect(0, 0, 50, 75) //x,y,width,height
     .fill({
     color: 0xff0000,
@@ -125,7 +85,7 @@ const rectangle = new Graphics()
     color: 0x000000,
     width: 6
     });
-app.stage.addChild(rectangle);
+app.stage.addChild(Player1);
 
 const Player2 = new Graphics()
         .rect(0, 0, 50, 75) //x,y,width,height
@@ -137,7 +97,9 @@ const Player2 = new Graphics()
         color: 0x000000,
         width: 6
         });
+app.stage.addChild(Player2);
 
+//testing how to slap an image on a sprite        
 const texture = await Assets.load('/images/sesu.png');
 const sprite = Sprite.from(texture);
 sprite.scale = 0.2
@@ -145,57 +107,61 @@ sprite.position._x = 250;
 sprite.position._y = 250;
 app.stage.addChild(sprite);
 
+//the grid
+const grid = new Graphics();
+    grid.lineStyle(3, 0x000000, 0.9);
+    for (let i = 0; i <= gridCols; i++) {
+        grid.moveTo(i * gridSize, 0);
+        grid.lineTo(i * gridSize, gridRows * gridSize);
+    }
+    for (let j = 0; j <= gridRows; j++) {
+        grid.moveTo(0, j * gridSize);
+        grid.lineTo(gridCols * gridSize, j * gridSize);
+    }
+app.stage.addChild(grid);
+
+//borders
+const borders = new Graphics();
+    borders.lineStyle(4, 0x000000);
+    borders.drawRect(0, 0, gridCols, gridSize, gridRows * gridSize);
+app.stage.addChild(borders);
+
 document.body.appendChild(app.canvas);
 
+    //ticker
 app.ticker.add(() => {
-
-    /*if (isColliding(rectangle, sprite)) {
-        rectangle.tint = 0x00ff00; // Change color on collision
-    }else {
-        rectangle.tint = 0xff0000; // Reset color
-    }*/
-
-    if (playercount==1){
-        if (isColliding(rectangle, sprite)){
-            console.log("colliding");
-            playercount++;
-            newPlayer();
-        }else{
-            console.log("not colliding");
-        }
+    //player 1 movement
+    Player1.x += p1velocityX * speedmodifier;
+    Player1.y += p1velocityY * speedmodifier;
+    if (Player1.x < 0){
+        Player1.x = maxWidht / 2 - Player1.width / 2;
     }
-
-    rectangle.x += p1velocityX * speedmodifier;
-    rectangle.y += p1velocityY * speedmodifier;
-    if (rectangle.x < 0){
-        rectangle.x = maxWidht / 2 - rectangle.width / 2;
+    if (Player1.x > maxWidht - Player1.width){
+        Player1.x = maxWidht / 2 - Player1.width / 2;
     }
-    if (rectangle.x > maxWidht - rectangle.width){
-        rectangle.x = maxWidht / 2 - rectangle.width / 2;
+    if (Player1.y < 0){
+        Player1.y = maxHeight/2 - Player1.height/2;
     }
-    if (rectangle.y < 0){
-        rectangle.y = maxHeight/2 - rectangle.height/2;
-    }
-    if (rectangle.y > maxHeight- rectangle.height){
-        rectangle.y = maxHeight/2 - rectangle.height/2;
+    if (Player1.y > maxHeight- Player1.height){
+        Player1.y = maxHeight/2 - Player1.height/2;
     } 
 
-    if (playercount ==2){
-        Player2.x += p2velocityX * speedmodifier;
-        Player2.y += p2velocityY * speedmodifier;
-        if (Player2.x < 0){
-            Player2.x = maxWidht / 2 - Player2.width / 2;
-        }
-        if (Player2.x > maxWidht - Player2.width){
-            Player2.x = maxWidht / 2 - Player2.width / 2;
-        }
-        if (Player2.y < 0){
-            Player2.y = maxHeight/2 - Player2.height/2;
-        }
-        if (Player2.y > maxHeight- Player2.height){
-            Player2.y = maxHeight/2 - Player2.height/2;
-        } 
+    //player 2 movement
+    Player2.x += p2velocityX * speedmodifier;
+    Player2.y += p2velocityY * speedmodifier;
+    if (Player2.x < 0){
+        Player2.x = maxWidht / 2 - Player2.width / 2;
     }
+    if (Player2.x > maxWidht - Player2.width){
+        Player2.x = maxWidht / 2 - Player2.width / 2;
+    }
+    if (Player2.y < 0){
+        Player2.y = maxHeight/2 - Player2.height/2;
+    }
+    if (Player2.y > maxHeight- Player2.height){
+        Player2.y = maxHeight/2 - Player2.height/2;
+    } 
+    
 });
 
 })();
